24c0a807a4c5aea108f9f0f0317e049104b3adb3
Karel Wintersky <karel.wintersky@yandex.ru>
2023-01-08 05:33:31 +0300

    2.0.5
    - [*] более или менее работающие middleware. Не работают для URL с опциональными частями

--- sources/AppRouter.php
+++ sources/AppRouter.php
@@ -354,8 +354,6 @@ class AppRouter implements AppRouterInterface
                     ? "{$rule['namespace']}\\{$rule['handler']}"
                     : $rule['handler'];
 
-                // sage($handler);
-                
                 $r->addRoute($rule['httpMethod'], $rule['route'], $handler);
             }
         });
@@ -365,10 +363,14 @@ class AppRouter implements AppRouterInterface
 
         list($state, $handler, $method_parameters) = $routeInfo;
 
+        //@todo: никак невозможно выяснить, какому правилу сопоставлен обработанный URL. Issue?
+
         // dispatch errors
         if ($state === FastRoute\Dispatcher::NOT_FOUND) {
+            // URL or URI? https://ru.wikipedia.org/wiki/URI
             throw new AppRouterNotFoundException(self::jsonize([
-                'message'   =>  "URL " . self::$uri . " not found",
+                'message'   =>  "URL not found",
+                'method'    =>  self::$httpMethod,
                 'uri'       =>  self::$uri
             ]), 404);
         }
@@ -383,10 +385,202 @@ class AppRouter implements AppRouterInterface
 
         //@todo: нужно получить параметры правила для обработанного роута!
 
-        sage($routeInfo);
+        $rules = self::getRoutingRules();
 
-        //@todo: namespace
+        // sage($rules);
 
+        $rules_key = self::$httpMethod . ' ' . self::$uri;
+
+        // sage($rules_key);
+
+        $rule = array_key_exists($rules_key, $rules) ? $rules[$rules_key] : [];
+
+        // sage($rule);
+
+        /*if ($handler instanceof \Closure) {
+            $actor = $handler;
+        } elseif (strpos($handler, '@') > 0) {
+            // dynamic method
+            list($class, $method) = explode('@', $handler, 2);
+
+            if (!class_exists($class)) {
+                self::$logger->error("Class {$class} not defined.", [ self::$uri, self::$httpMethod, $class ]);
+                throw new AppRouterHandlerError(self::jsonize([
+                    'message'   =>  "Class {$class} not defined",
+                    'uri'       =>  self::$uri,
+                    'method'    =>  self::$httpMethod,
+                    'info'      =>  self::$routeInfo
+                ]), 500);
+            }
+
+            if (!method_exists($class, $method)) {
+                self::$logger->error("Method {$method} not declared at {$class} class.", [ self::$uri, self::$httpMethod, $class ]);
+                throw new AppRouterHandlerError(self::jsonize([
+                    'message'   =>  "Method {$method} not declared at {$class} class",
+                    'uri'       =>  self::$uri,
+                    'method'    =>  self::$httpMethod,
+                    'info'      =>  self::$routeInfo
+                ]), 500);
+            }
+
+            $actor = [ new $class, $method ];
+
+        } elseif (strpos($handler, '::')) {
+            // static method
+            list($class, $method) = explode('::', $handler, 2);
+
+            if (!class_exists($class)){
+                self::$logger->error("Class {$class} not defined.", [ self::$uri, self::$httpMethod, $class ]);
+                throw new AppRouterHandlerError(self::jsonize([
+                    'message'   =>  "Class {$class} not defined",
+                    'uri'       =>  self::$uri,
+                    'method'    =>  self::$httpMethod,
+                    'info'      =>  self::$routeInfo
+                ]), 500);
+            }
+
+            if (!method_exists($class, $method)){
+                self::$logger->error("Method {$method} not declared at {$class} class", [ self::$uri, self::$httpMethod, $class ]);
+                throw new AppRouterHandlerError(self::jsonize([
+                    'message'   =>  "Method {$method} not declared at {$class} class",
+                    'uri'       =>  self::$uri,
+                    'method'    =>  self::$httpMethod,
+                    'info'      =>  self::$routeInfo
+                ]), 500);
+            }
+
+            $actor = [ $class, $method ];
+
+        } else {
+            // function
+            if (!function_exists($handler)){
+                self::$logger->error("Handler function {$handler} not found", [ self::$uri, self::$httpMethod, $handler ]);
+                throw new AppRouterHandlerError(self::jsonize([
+                    'message'   =>  "Handler function {$handler} not found",
+                    'uri'       =>  self::$uri,
+                    'method'    =>  self::$httpMethod,
+                    'info'      =>  self::$routeInfo
+                ]), 500);
+            }
+
+            $actor = $handler;
+        }*/
+        $actor = self::makeHandler($handler);
+
+        if (self::is_handler($rule['middlewares']['before'])){
+            $before = self::makeHandler($rule['middlewares']['before']);
+            $before();
+        }
+
+        call_user_func_array($actor, $method_parameters);
+
+        if (self::is_handler($rule['middlewares']['after'])){
+            $after = self::makeHandler($rule['middlewares']['after']);
+            $after();
+        }
+
+        unset($state);
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @return array
+     */
+    public static function getRoutingInfo(): array
+    {
+        return self::$routeInfo;
+    }
+
+    /**
+     * Возвращает список объявленных роутов: [ 'method route' => [ handler, namespace, name ]
+     *
+     * @return array
+     */
+    public static function getRoutingRules(): array
+    {
+        $rules = [];
+        foreach (self::$rules as $record) {
+            $key = is_array($record['httpMethod']) ? "ANY {$record['route']}" : "{$record['httpMethod']} {$record['route']}";
+
+            if (array_key_exists($key, $rules)) {
+                $key .= " [ DUPLICATE ROUTE " . microtime(false) . ' ]';
+            }
+
+            $rules[ $key ] = [
+                'handler'   =>  is_callable($record['handler']) ? "Closure" : $record['handler'],
+                'namespace' =>  $record['namespace'],
+                'name'      =>  $record['name'],
+                'middlewares'   =>  [
+                    'before'        =>  $record['middlewares']['before'],
+                    'after'         =>  $record['middlewares']['after']
+                ]
+            ];
+        }
+
+        return $rules;
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    private static function jsonize($data)
+    {
+        return json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_INVALID_UTF8_SUBSTITUTE | JSON_THROW_ON_ERROR);
+    }
+
+    /**
+     * Выясняет, является ли передаваемый аргумент допустимым хэндлером
+     *
+     * @param $handler
+     * @param bool $validate_handlers
+     *
+     * @return bool
+     */
+    public static function is_handler($handler = null, $validate_handlers = false)
+    {
+        if (is_null($handler)) {
+            return false;
+        } elseif ($handler instanceof \Closure) {
+            return true;
+        } elseif (strpos($handler, '@') > 0) {
+            // dynamic method
+            list($class, $method) = explode('@', $handler, 2);
+
+            if ($validate_handlers && !class_exists($class)) {
+                return false;
+            }
+
+            if ($validate_handlers && !method_exists($class, $method)) {
+                return false;
+            }
+
+            return true;
+        } elseif (strpos($handler, '::')) {
+            // static method
+            list($class, $method) = explode('::', $handler, 2);
+
+            if ($validate_handlers && !class_exists($class)){
+                return false;
+            }
+
+            if ($validate_handlers && !method_exists($class, $method)){
+                return false;
+            }
+
+            return true;
+        } else {
+            // function
+            if ($validate_handlers && !function_exists($handler)){
+                return false;
+            }
+
+            return true;
+        }
+    } // is_handler()
+
+    private static function makeHandler($handler)
+    {
         if ($handler instanceof \Closure) {
             $actor = $handler;
         } elseif (strpos($handler, '@') > 0) {
@@ -456,107 +650,8 @@ class AppRouter implements AppRouterInterface
             $actor = $handler;
         }
 
-        /*if (self::is_handler()) {
-
-        }*/
-
-        sage($method_parameters);
-
-        call_user_func_array($actor, $method_parameters);
-
-        unset($state);
+        return $actor;
     }
-
-    /**
-     * @inheritDoc
-     *
-     * @return array
-     */
-    public static function getRoutingInfo(): array
-    {
-        return self::$routeInfo;
-    }
-
-    /**
-     * Возвращает список объявленных роутов: [ 'method route' => [ handler, namespace, name ]
-     *
-     * @return array
-     */
-    public static function getRoutingRules(): array
-    {
-        $rules = [];
-        foreach (self::$rules as $record) {
-            $key = is_array($record['httpMethod']) ? "ANY {$record['route']}" : "{$record['httpMethod']} {$record['route']}";
-
-            if (array_key_exists($key, $rules)) {
-                $key .= " [ DUPLICATE ROUTE " . microtime(false) . ' ]';
-            }
-
-            $rules[ $key ] = [
-                'handler'   =>  is_callable($record['handler']) ? "Closure" : $record['handler'],
-                'namespace' =>  $record['namespace'],
-                'name'      =>  $record['name'],
-            ];
-        }
-
-        return $rules;
-    }
-
-    /**
-     * @throws \JsonException
-     */
-    private static function jsonize($data)
-    {
-        return json_encode($data, JSON_UNESCAPED_UNICODE | JSON_PRESERVE_ZERO_FRACTION | JSON_INVALID_UTF8_SUBSTITUTE | JSON_THROW_ON_ERROR);
-    }
-
-    /**
-     * Выясняет, является ли передаваемый аргумент допустимым хэндлером
-     *
-     * @param $handler
-     * @param bool $validate_handlers
-     *
-     * @return bool
-     */
-    public static function is_handler($handler, $validate_handlers = false)
-    {
-        if ($handler instanceof \Closure) {
-            return true;
-        } elseif (strpos($handler, '@') > 0) {
-            // dynamic method
-            list($class, $method) = explode('@', $handler, 2);
-
-            if ($validate_handlers && !class_exists($class)) {
-                return false;
-            }
-
-            if ($validate_handlers && !method_exists($class, $method)) {
-                return false;
-            }
-
-            return true;
-        } elseif (strpos($handler, '::')) {
-            // static method
-            list($class, $method) = explode('::', $handler, 2);
-
-            if ($validate_handlers && !class_exists($class)){
-                return false;
-            }
-
-            if ($validate_handlers && !method_exists($class, $method)){
-                return false;
-            }
-
-            return true;
-        } else {
-            // function
-            if ($validate_handlers && !function_exists($handler)){
-                return false;
-            }
-
-            return true;
-        }
-    } // is_handler()
 
 }
 
